6bcd1de5e0d849939e19968b7a252567bf17276b
vyshnavip1998 <118480225+vyshnavip1998@users.noreply.github.com>
2022-11-18 14:55:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,153 @@
+import { useEffect, useState } from "react";
+import { fabric } from "fabric";
+import "./App.css";
+import black from "./components/Black.svg";
+import blue from "./components/Blue.svg";
+import check from "./components/Check.svg";
+import eraser from "./components/Eraser.svg";
+import green from "./components/Green.svg";
+import pen from "./components/Pen.svg";
+import red from "./components/Red.svg";
+import yellow from "./components/Yellow.svg";
+import slider from "./components/slider.svg";
+import Pages  from "./components/Pages/Pages";
+
+function App() {
+    const [canvas, setCanvas] = useState(null);
+    const [color, setColor] = useState("black");
+    const [isDomReady, setIsDomReady] = useState(false);
+    const [penSize, setPenSize] = useState(3);
+    const [eraserSize, setEraserSize] = useState(3);
+    const [invert, setInvert] = useState(false);
+
+    const initCanvas = () => {
+        const can = new fabric.Canvas("canvas");
+        const dom_canvas = document.querySelector(".canvas");
+        // console.log(dom_canvas.offsetHeight);
+        // console.log(dom_canvas.offsetWidth);
+        // can.setHeight(dom_canvas.offsetHeight);
+        // can.setWidth(dom_canvas.offsetWidth);
+        can.setHeight(window.innerHeight);
+        can.setWidth(window.innerWidth);
+
+        can.isDrawingMode = true;
+        return can;
+    };
+
+    useEffect(() => {
+        setCanvas(initCanvas());
+    }, []);
+
+    useEffect(() => {
+        if (canvas != null) {
+            canvas.freeDrawingBrush.color = color;
+        }
+    }, [color]);
+
+    useEffect(() => {
+        if (canvas != null) {
+            // canvas.freeDrawingBrush.color = color;
+            canvas.freeDrawingBrush.invert = invert;
+            console.log({ invert });
+        }
+    }, [invert]);
+
+    useEffect(() => {
+        if (canvas != null) {
+            canvas.freeDrawingBrush.width = penSize;
+            console.log(penSize);
+        }
+    }, [penSize]);
+
+    return (
+        <div className="App">
+            <div className="tools">
+                <div className="pen-tools">
+                    <div className="pen">
+                        <img alt="pen" src={pen} />
+                    </div>
+
+                    <input
+                        type="range"
+                        className="slider"
+                        value={penSize}
+                        onChange={(e) => setPenSize(e.target.value)}
+                        min="1"
+                        max="10"
+                        id="myRange"
+                    />
+
+                    <div
+                        className="color black"
+                        onClick={() => setColor("black")}
+                    >
+                        <img alt="" src={black} />
+                        {color === "black" ? (
+                            <img alt="check" className="check" src={check} />
+                        ) : null}
+                    </div>
+                    <div
+                        className="color green"
+                        onClick={() => setColor("green")}
+                    >
+                        <img alt="green" src={green} />
+                        {color === "green" ? (
+                            <img alt="check" className="check" src={check} />
+                        ) : null}
+                    </div>
+                    <div
+                        className="color yellow"
+                        onClick={() => setColor("yellow")}
+                    >
+                        <img alt="yellow" src={yellow} />
+
+                        {color === "yellow" ? (
+                            <img alt="check" className="check" src={check} />
+                        ) : null}
+                    </div>
+                    <div
+                        className="color blue"
+                        onClick={() => setColor("blue")}
+                    >
+                        <img alt="blue" src={blue} />
+
+                        {color === "blue" ? (
+                            <img alt="check" className="check" src={check} />
+                        ) : null}
+                    </div>
+                    <div className="color red" onClick={() => setColor("red")}>
+                        <img alt="red" src={red} />
+
+                        {color === "red" ? (
+                            <img alt="check" className="check" src={check} />
+                        ) : null}
+                    </div>
+                </div>
+                <div className="pen-settings">
+                    <div
+                        className="eraser"
+                        onClick={() => {
+                            setInvert(!invert);
+                        }}
+                    >
+                        <img alt="eraser" src={eraser} />
+                    </div>
+
+                    <input
+                        type="range"
+                        min="1"
+                        max="100"
+                        id="myRange"
+                        className="slider"
+                    />
+                </div>
+            </div>
+          
+            {/* <div className="canvas"> */}
+            <canvas id="canvas"></canvas>
+            {/* </div> */}
+        </div>
+    );
+}
+
+export default App;
